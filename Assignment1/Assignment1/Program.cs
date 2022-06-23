08606c7753be47f3c75337f0b65aebc4a96764ac
Ahlam-N <107494447+Ahlam-N@users.noreply.github.com>
2022-06-23 18:10:43 +0300

    Assignment 1
    Finding the number of 1's

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Program to count the number of 1's
+namespace assignments
+{
+    public class numberofones
+    {
+        public static void Main(String[] args)
+        {
+            int m;
+            int count = 0;
+            Console.WriteLine("Enter the limit: ");
+            m = int.Parse(Console.ReadLine());
+            int[] a = new int[m];
+            Console.WriteLine("Enter the numbers: ");
+            for (int i = 0; i < m; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach (int o in a)
+            {
+                if (o == 1)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("The number of 1's in the Entered Number : ");
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
