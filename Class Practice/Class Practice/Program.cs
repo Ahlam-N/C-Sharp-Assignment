31fa62bc127de55326aa98f00993c3102da88c0a
Ahlam-N <107494447+Ahlam-N@users.noreply.github.com>
2022-06-30 10:09:17 +0300

    latest commit

---
+++ Class Practice/Class Practice/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Employee
+{
+    private string _name;
+    public string name
+    {
+        get => _name; 
+        set => _name = value; 
+    }
+}
+class program
+{
+    public static void Main (string[] args)
+    {
+        Employee emp = new Employee();
+        emp.name = "Sumeya";
+       Console.WriteLine(emp.name);
+      // emp.name=Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine
+    }
+}
---
+++ Class practice shape/Class practice shape/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+public interface  shape
+{
+    public double getArea();
+  public double getPerimeter();
+   
+}
+class circle: shape
+{
+    static double PI = 3.14;
+    int _radius;
+    public int radius
+    {
+        get => _radius;
+        set => _radius = value;
+    }
+    //public int MyProperty { get; set; }
+    public double getArea()
+    {
+        return PI * (radius * radius);
+
+    }
+    public double getPerimeter()
+    {
+        return 2 * PI * radius
+            ;
+    }
+   
+}
+class square:shape
+{
+    int _length;
+    int _width;
+    public int length
+
+    {
+        get { return _length; }
+        set { _length = value; }
+    } 
+
+public int width
+    {
+        get { return _width; }
+        set { _width = value; }
+    }
+    public double getArea()
+    {
+        return width*length;
+
+    }
+   
+
+}
+    class triangle
+    {
+        int _base;
+        int _height;
+    public int base1{
+        get => _base;
+        set => _base = value;
+        }
+    public int height
+    {
+        get => _height; 
+            set => _height = value;
+    }
+
+
+    }
+
